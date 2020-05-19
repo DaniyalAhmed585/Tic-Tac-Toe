f7a35c6534910276cbd927aa55aa0f9cc3643895
DaniyalAhmed585 <daniyalahmed1010@gmail.com>
2020-05-20 02:02:41 +0500

    Ready to Commit

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Game
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.Rd2 = new System.Windows.Forms.RadioButton();
+            this.Rd1 = new System.Windows.Forms.RadioButton();
+            this.Pl2 = new System.Windows.Forms.Label();
+            this.Pl1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Heading = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.b9);
+            this.panel1.Controls.Add(this.b8);
+            this.panel1.Controls.Add(this.b7);
+            this.panel1.Controls.Add(this.b6);
+            this.panel1.Controls.Add(this.b5);
+            this.panel1.Controls.Add(this.b4);
+            this.panel1.Controls.Add(this.b3);
+            this.panel1.Controls.Add(this.b2);
+            this.panel1.Controls.Add(this.b1);
+            this.panel1.Controls.Add(this.Rd2);
+            this.panel1.Controls.Add(this.Rd1);
+            this.panel1.Controls.Add(this.Pl2);
+            this.panel1.Controls.Add(this.Pl1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.panel1.Location = new System.Drawing.Point(12, 82);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(650, 407);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(556, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Made By Daniyal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Sienna;
+            this.label2.Location = new System.Drawing.Point(519, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Sienna;
+            this.label1.Location = new System.Drawing.Point(297, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 13;
+            // 
+            // b9
+            // 
+            this.b9.AutoSize = true;
+            this.b9.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.ForeColor = System.Drawing.Color.Maroon;
+            this.b9.Location = new System.Drawing.Point(398, 278);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(134, 92);
+            this.b9.TabIndex = 12;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // b8
+            // 
+            this.b8.AutoSize = true;
+            this.b8.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.ForeColor = System.Drawing.Color.Maroon;
+            this.b8.Location = new System.Drawing.Point(269, 278);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(134, 92);
+            this.b8.TabIndex = 11;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // b7
+            // 
+            this.b7.AutoSize = true;
+            this.b7.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.ForeColor = System.Drawing.Color.Maroon;
+            this.b7.Location = new System.Drawing.Point(140, 278);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(134, 92);
+            this.b7.TabIndex = 10;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // b6
+            // 
+            this.b6.AutoSize = true;
+            this.b6.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.ForeColor = System.Drawing.Color.Maroon;
+            this.b6.Location = new System.Drawing.Point(398, 192);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(134, 92);
+            this.b6.TabIndex = 9;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // b5
+            // 
+            this.b5.AutoSize = true;
+            this.b5.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.ForeColor = System.Drawing.Color.Maroon;
+            this.b5.Location = new System.Drawing.Point(269, 192);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(134, 92);
+            this.b5.TabIndex = 8;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // b4
+            // 
+            this.b4.AutoSize = true;
+            this.b4.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.ForeColor = System.Drawing.Color.Maroon;
+            this.b4.Location = new System.Drawing.Point(140, 192);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(134, 92);
+            this.b4.TabIndex = 7;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // b3
+            // 
+            this.b3.AutoSize = true;
+            this.b3.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.b3.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.ForeColor = System.Drawing.Color.Maroon;
+            this.b3.Location = new System.Drawing.Point(398, 103);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(134, 92);
+            this.b3.TabIndex = 6;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // b2
+            // 
+            this.b2.AutoSize = true;
+            this.b2.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.ForeColor = System.Drawing.Color.Maroon;
+            this.b2.Location = new System.Drawing.Point(269, 103);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(134, 92);
+            this.b2.TabIndex = 5;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // b1
+            // 
+            this.b1.AutoSize = true;
+            this.b1.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.ForeColor = System.Drawing.Color.Maroon;
+            this.b1.Location = new System.Drawing.Point(140, 103);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(134, 92);
+            this.b1.TabIndex = 4;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Rd2
+            // 
+            this.Rd2.AutoSize = true;
+            this.Rd2.Location = new System.Drawing.Point(356, 55);
+            this.Rd2.Name = "Rd2";
+            this.Rd2.Size = new System.Drawing.Size(14, 13);
+            this.Rd2.TabIndex = 3;
+            this.Rd2.TabStop = true;
+            this.Rd2.UseVisualStyleBackColor = true;
+            // 
+            // Rd1
+            // 
+            this.Rd1.AutoSize = true;
+            this.Rd1.Checked = true;
+            this.Rd1.Location = new System.Drawing.Point(140, 55);
+            this.Rd1.Name = "Rd1";
+            this.Rd1.Size = new System.Drawing.Size(14, 13);
+            this.Rd1.TabIndex = 2;
+            this.Rd1.TabStop = true;
+            this.Rd1.UseVisualStyleBackColor = true;
+            // 
+            // Pl2
+            // 
+            this.Pl2.AutoSize = true;
+            this.Pl2.Font = new System.Drawing.Font("Bradley Hand ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pl2.Location = new System.Drawing.Point(376, 48);
+            this.Pl2.Name = "Pl2";
+            this.Pl2.Size = new System.Drawing.Size(136, 24);
+            this.Pl2.TabIndex = 1;
+            this.Pl2.Text = "Player 2 Turn\r\n";
+            // 
+            // Pl1
+            // 
+            this.Pl1.AutoSize = true;
+            this.Pl1.Font = new System.Drawing.Font("Bradley Hand ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pl1.Location = new System.Drawing.Point(160, 50);
+            this.Pl1.Name = "Pl1";
+            this.Pl1.Size = new System.Drawing.Size(134, 24);
+            this.Pl1.TabIndex = 0;
+            this.Pl1.Text = "Player 1 Turn\r\n";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Bisque;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.Heading);
+            this.panel2.Location = new System.Drawing.Point(12, 22);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(650, 77);
+            this.panel2.TabIndex = 1;
+            // 
+            // Heading
+            // 
+            this.Heading.AutoSize = true;
+            this.Heading.Font = new System.Drawing.Font("Ravie", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Heading.Location = new System.Drawing.Point(191, 12);
+            this.Heading.Name = "Heading";
+            this.Heading.Size = new System.Drawing.Size(287, 43);
+            this.Heading.TabIndex = 0;
+            this.Heading.Text = "TIC TAC TOE";
+            this.Heading.Click += new System.EventHandler(this.Heading_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(674, 501);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GAME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label Heading;
+        private System.Windows.Forms.RadioButton Rd2;
+        private System.Windows.Forms.RadioButton Rd1;
+        private System.Windows.Forms.Label Pl2;
+        private System.Windows.Forms.Label Pl1;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Rd1.Checked == true)
+            {
+                b1.Text = "O";
+                Rd2.Checked = true;
+            }
+            else if (Rd2.Checked == true)
+            {
+                b1.Text = "X";
+                Rd1.Checked = true;
+            }
+            b1.Enabled = false;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Rd1.Checked == true)
+            {
+                b2.Text = "O";
+                Rd2.Checked = true;
+            }
+            else if (Rd2.Checked == true)
+            {
+                b2.Text = "X";
+                Rd1.Checked = true;
+            }
+            b2.Enabled = false;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (Rd1.Checked == true)
+            {
+                b9.Text = "O";
+                Rd2.Checked = true;
+            }
+            else if (Rd2.Checked == true)
+            {
+                b9.Text = "X";
+                Rd1.Checked = true;
+            }
+            b9.Enabled = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (Rd1.Checked == true)
+            {
+                b8.Text = "O";
+                Rd2.Checked = true;
+            }
+            else if (Rd2.Checked == true)
+            {
+                b8.Text = "X";
+                Rd1.Checked = true;
+            }
+            b8.Enabled = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (Rd1.Checked == true)
+            {
+                b7.Text = "O";
+                Rd2.Checked = true;
+            }
+            else if (Rd2.Checked == true)
+            {
+                b7.Text = "X";
+                Rd1.Checked = true;
+            }
+            b7.Enabled = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (Rd1.Checked == true)
+            {
+                b6.Text = "O";
+                Rd2.Checked = true;
+            }
+            else if (Rd2.Checked == true)
+            {
+                b6.Text = "X";
+                Rd1.Checked = true;
+            }
+            b6.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (Rd1.Checked == true)
+            {
+                b5.Text = "O";
+                Rd2.Checked = true;
+            }
+            else if (Rd2.Checked == true)
+            {
+                b5.Text = "X";
+                Rd1.Checked = true;
+            }
+            b5.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (Rd1.Checked == true)
+            {
+                b4.Text = "O";
+                Rd2.Checked = true;
+            }
+            else if (Rd2.Checked == true)
+            {
+                b4.Text = "X";
+                Rd1.Checked = true;
+            }
+            b4.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Rd1.Checked == true)
+            {
+                b3.Text = "O";
+                Rd2.Checked = true;
+            }
+            else if (Rd2.Checked == true)
+            {
+                b3.Text = "X";
+                Rd1.Checked = true;
+            }
+            b3.Enabled = false;
+        }
+        int a = 0, b = 0;
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (b1.Text != ""||b2.Text!=""||b3.Text!=""||b4.Text!=""||b5.Text!=""||b6.Text!=""||b7.Text!=""||b8.Text!=""||b9.Text!="")
+            {
+                int i = 0;
+                int y = 0;
+                if (label1.Text == "")
+                    i = 1;
+                else if (label1.Text != "")
+                {
+                    i = Convert.ToInt32 (label1.Text) + 1;
+                    a = i;
+                }
+
+                int j = 0;
+                if (label2.Text == "")
+                    j = 1;
+                else if (label2.Text != "")
+                {
+                    j = Convert.ToInt32(label2.Text) + 1;
+                    b = j;
+                }
+                DialogResult dr = new DialogResult();
+                if (b1.Text == "O" && b2.Text == "O" && b3.Text == "O" || b4.Text == "O" && b5.Text == "O" && b6.Text == "O"
+                    || b7.Text == "O" && b8.Text == "O" && b9.Text == "O" || b1.Text == "O" && b4.Text == "O" && b7.Text == "O"
+                    || b2.Text == "O" && b5.Text == "O" && b8.Text == "O" || b3.Text == "O" && b6.Text == "O" && b9.Text == "O"
+                    || b1.Text == "O" && b5.Text == "O" && b9.Text == "O" || b3.Text == "O" && b5.Text == "O" && b7.Text == "O")
+                {
+                    dr = MessageBox.Show("            " + Form1.setvaluefortext1 +" Wins This Round", "                     Round Result", MessageBoxButtons.RetryCancel);
+                    if (dr == System.Windows.Forms.DialogResult.Retry)
+                    {
+
+                        b1.Text = b2.Text = b3.Text = b4.Text = b5.Text = b6.Text = b7.Text = b8.Text = b9.Text = "";
+                        b1.Enabled = b2.Enabled = b3.Enabled = b4.Enabled = b5.Enabled = b6.Enabled = b7.Enabled = b8.Enabled = b9.Enabled = true;
+                        Rd1.Checked = true;
+                        label1.Text = Convert.ToString(i);
+                    }
+                    else if (dr == System.Windows.Forms.DialogResult.Cancel)
+                    {
+                        Application.Exit();
+                    }
+                }
+                else if (b1.Text == "X" && b2.Text == "X" && b3.Text == "X" || b4.Text == "X" && b5.Text == "X" && b6.Text == "X"
+                    || b7.Text == "X" && b8.Text == "X" && b9.Text == "X" || b1.Text == "X" && b4.Text == "X" && b7.Text == "X"
+                    || b2.Text == "X" && b5.Text == "X" && b8.Text == "X" || b3.Text == "X" && b6.Text == "X" && b9.Text == "X"
+                    || b1.Text == "X" && b5.Text == "X" && b9.Text == "X" || b3.Text == "X" && b5.Text == "X" && b7.Text == "X")
+                {
+                    dr = MessageBox.Show("            "+ Form1.setvaluefortext2 + " Wins This Round", "                     Round Result", MessageBoxButtons.RetryCancel);
+                    if (dr == System.Windows.Forms.DialogResult.Retry)
+                    {
+                        b1.Text = b2.Text = b3.Text = b4.Text = b5.Text = b6.Text = b7.Text = b8.Text = b9.Text = "";
+                        b1.Enabled = b2.Enabled = b3.Enabled = b4.Enabled = b5.Enabled = b6.Enabled = b7.Enabled = b8.Enabled = b9.Enabled = true;
+                        Rd2.Checked = true;
+                        label2.Text = Convert.ToString(j);
+                    }
+                    else if (dr == System.Windows.Forms.DialogResult.Cancel)
+                    {
+                        Application.Exit();
+                    }
+                }
+                else if (b1.Text != "" && b2.Text != "" && b3.Text != "" && b4.Text != "" && b5.Text != "" && b6.Text != "" && b7.Text != "" && b8.Text != "" && b9.Text != "")
+                {
+                    if (y == 0)
+                    {
+                        MessageBox.Show("It's a Tie", "Round Result", MessageBoxButtons.OK);
+                        b1.Text = b2.Text = b3.Text = b4.Text = b5.Text = b6.Text = b7.Text = b8.Text = b9.Text = "";
+                        b1.Enabled = b2.Enabled = b3.Enabled = b4.Enabled = b5.Enabled = b6.Enabled = b7.Enabled = b8.Enabled = b9.Enabled = true;
+                        Rd1.Checked = true;
+                    }
+                }
+            }
+            else if (b1.Text == "" && b2.Text == "" && b3.Text == "" && b4.Text == "" && b5.Text == "" && b6.Text == "" && b7.Text == "" && b8.Text == "" && b9.Text == "")
+            {
+                if (a > 4 && Convert.ToInt32(label1.Text) > 4)
+                {
+                    MessageBox.Show(Form1.setvaluefortext1 + " Wins", "Final Result", MessageBoxButtons.OK);
+                    Application.Exit();
+                }
+                if (b > 4 && Convert.ToInt32(label2.Text) > 4)
+                {
+                    MessageBox.Show(Form1.setvaluefortext2 + " Wins", "Final Result", MessageBoxButtons.OK);
+                    Application.Exit();
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Pl1.Text = Form1.setvaluefortext1;
+            Pl2.Text = Form1.setvaluefortext2;
+        }
+
+        private void b1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Heading_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
